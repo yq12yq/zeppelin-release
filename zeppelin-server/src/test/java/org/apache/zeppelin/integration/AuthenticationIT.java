ecefd17ce90e64d0d7416cd405738f9987ce2f9d
Prabhjyot Singh <prabhjyotsingh@gmail.com>
2017-04-29 23:21:38 +0530

    BUG-80227 Improve Session #2302 fix
    ### What is this PR for?
    After the merge of #2302 breaks authentication.
    
    ### What type of PR is it?
    [Hot Fix]
    
    ### Todos
    * [x] - Fix selenium logout issue (flaky test)
    
    ### Questions:
    * Does the licenses files need update?
    * Is there breaking changes for older versions?
    * Does this needs documentation?
    
    Author: Prabhjyot Singh <prabhjyotsingh@gmail.com>
    
    Closes #2304 from prabhjyotsingh/hotfix/ImproveSession and squashes the following commits:
    
    94ba9e774 [Prabhjyot Singh] check if modal window is present, and then close it
    f993b59fa [Prabhjyot Singh] improve session

--- zeppelin-server/src/test/java/org/apache/zeppelin/integration/AuthenticationIT.java
+++ zeppelin-server/src/test/java/org/apache/zeppelin/integration/AuthenticationIT.java
@@ -16,6 +16,14 @@
  */
 package org.apache.zeppelin.integration;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.zeppelin.AbstractZeppelinIT;
